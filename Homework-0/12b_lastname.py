4c8db74c8f03e83a4446e8187c8870f17f01ec50
MathA77 <fall14.matha77@gmail.com>
2014-09-14 20:24:14 -0400

    solution example

--- Homework-0/12b_lastname.py
+++ Homework-0/12b_lastname.py
@@ -1,15 +1,19 @@
-#This is an example of a solution to a homework problem in python
-#at the top of the script will be the author and any contributors
+'''
+This is an example of a solution to a homework problem in python
+at the top of the script will be the author and any contributors
+'''
 
 
 # 1. restate the problem (cut/paste) in comments
 
 '''Create a grid of x values for plotting x = [-10: .1 : 20]'''
 
-# 2. post solution
+
+# 2. post solution code
 
 import numpy as np
 x = np.arange(-10, 20.1, .1)
+
 
 # 3. write pseudocode/plain english explanation
 
